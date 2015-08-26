bf0297a273a6a19a392d57868e597fda15ddacf8
huyongli <huyongli1989@gmail.com>
2015-08-26 15:55:50 +0800

    解决数据库创建时onCreate方法中不能操作db的bug

--- TigerDB/gen/cn/ittiger/R.java
+++ TigerDB/gen/cn/ittiger/R.java
@@ -11,10 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static int ic_launcher=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static int app_name=0x7f030000;
     }
     public static final class style {
         /** 
@@ -38,10 +38,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static int AppBaseTheme=0x7f040000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static int AppTheme=0x7f040001;
     }
 }
